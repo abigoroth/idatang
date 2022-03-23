7d7783fb1debe0f7232c0d950610b65c0f9a288e
yakob ubaidi <yakob.ubaidi@gmail.com>
2022-03-23 19:30:56 +0800

    check csrff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery unless: -> { ENV["RAILS_ENV"] == "development" }
+  before_action :check_csrff
+
+  def check_csrff
+    Rails.logger.info "@@@ check_csrff authenticity_token #{params[:authenticity_token]}"
+  end
 end
