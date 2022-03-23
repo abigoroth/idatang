6374e7ada5ae5d73471ecd7dd9964b7e7454937f
yakob ubaidi <yakob.ubaidi@gmail.com>
2022-03-23 19:40:16 +0800

    protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery unless: -> { ENV["RAILS_ENV"] == "development" }
-  before_action :check_csrff
-
-  def check_csrff
-    Rails.logger.info "@@@ check_csrff authenticity_token #{params[:authenticity_token]}"
-  end
+  # TODO: always aware to make any other form submission in future, eg registration etc.
+  protect_from_forgery
 end
